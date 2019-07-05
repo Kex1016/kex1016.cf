4621d958c00e73d2fc25a9b83a4c4419156bbfe8
Kex1016 <nyilasdavid@gmail.com>
2019-07-05 23:58:10 +0200

    literally redone entire site
    (totally not stolen from carrd)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function Load() {
     var sub1 = document.getElementById("sub");
-    var sub = ["2D > 3D", "I like fictional characters.", "When in doubt just watch anime.", "Yuri connoisseour.", "This is my site. Yay.", "I'm just a nolife.", "Why are you reading these?","This is an easter egg. Now send it to me.","I hope you're having a great day.","<a href='https://anilist.co/character/82525/Shiro'>Shiro</a> best girl."];
+    var sub = ["2D > 3D", "I like fictional characters.", "When in doubt just watch anime.", "Yuri connoisseour.", "This is my site. Yay.", "I'm just a nolife.", "Why are you reading these?","This is an easter egg. Now send it to me.","I hope you're having a great day.","<a href='https://anilist.co/character/19565/Yui-Hirasawa' target='_blank'>Yui</a> best girl."];
     var subR = Math.floor((Math.random() * 10));
     sub1.innerHTML = sub[subR];
     Subtitle();
@@ -18,7 +18,7 @@ function Subtitle() {
         
         setTimeout(function () {
             if(sub[subR]==69){
-            sub1.innerHTML = "You've watched " + subLUL + " subtitles go by.";
+            sub1.innerHTML = "You've watched " + subLUL + " of these go by.";
             sub1.style.opacity="1";
             } else {
             sub1.innerHTML = sub[subR];

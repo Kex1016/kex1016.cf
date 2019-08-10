642e404c46b059c0b21b75f5bf6aaa2e6b5fd6d6
Kex1016 <nyilasdavid@gmail.com>
2019-08-11 01:11:01 +0200

    Reworked the AniList page and fixed sidebar on mobile

--- js/graphql.js
+++ js/graphql.js
@@ -26,7 +26,7 @@ var query = `query ($id: Int){
       large
     }
     favourites {
-      anime(page: 1, perPage: 10) {
+      anime(page: 1, perPage: 40) {
         nodes {
           title {
             romaji
@@ -79,6 +79,7 @@ function handleData(obj) {
 	console.log(obj);
 
 	document.getElementById("gql-uname").innerHTML = obj.data.User.name;
+	document.getElementById("gql-topanime").innerHTML = obj.data.User.favourites.anime.nodes.length;
 	document.getElementById("gql-avatar").src = obj.data.User.avatar.large;
 	var i = 0;
 	var num = 1;
@@ -90,29 +91,30 @@ function handleData(obj) {
 			</li>
 		*/
 		if (obj.data.User.favourites.anime.nodes[i].title.english == null) {
-			document.getElementById("gql-alist").innerHTML += "<li><div class='collapsible-header' style='color: #263238;'><span class='gql-anumber'>" + num + "</span>&nbsp;&nbsp;&nbsp;" + obj.data.User.favourites.anime.nodes[i].title.romaji + "</div><div class='collapsible-body' style='background: #fff; color: #263238;'><div class='card'><div class='card-content' style='background: " + obj.data.User.favourites.anime.nodes[i].coverImage.color + "'><span class='card-title'>" + obj.data.User.favourites.anime.nodes[i].title.romaji +"</span></div><div class='card-content'><p><img width='17%' src='" + obj.data.User.favourites.anime.nodes[i].coverImage.extraLarge + "' style='overflow:auto; float:left; margin-right:1%;'>" + obj.data.User.favourites.anime.nodes[i].description.replace('\r\n','') + "<p></p><h6 style='text-decoration: underline;'>My notes on the anime:</h6><p id='gql-notes" + num + "'></p></p></div><div class='card-action'><a href='" + obj.data.User.favourites.anime.nodes[i].siteUrl + "'>Go to anime's page</a></div></div>"
+			document.getElementById("gql-alist").innerHTML += "<li><div class='collapsible-header' style='color: #263238;'><span class='gql-anumber'>" + num + "</span>&nbsp;&nbsp;&nbsp;" + obj.data.User.favourites.anime.nodes[i].title.romaji + "</div><div class='collapsible-body' style='background: #fff; color: #263238;'><div class='card'><div class='card-content' style='background: " + obj.data.User.favourites.anime.nodes[i].coverImage.color + "'><span class='card-title'>" + obj.data.User.favourites.anime.nodes[i].title.romaji + "</span></div><div class='card-content'><p><img width='17%' src='" + obj.data.User.favourites.anime.nodes[i].coverImage.extraLarge + "' style='overflow:auto; float:left; margin-right:1%;'>" + obj.data.User.favourites.anime.nodes[i].description.replace('\r\n', '') + "<p></p><h6 style='text-decoration: underline;'>My notes on the anime:</h6><p id='gql-notes" + num + "'></p></p></div><div class='card-action'><a href='" + obj.data.User.favourites.anime.nodes[i].siteUrl + "'>Go to anime's page</a></div></div>"
+			num++;
 		} else {
-			document.getElementById("gql-alist").innerHTML += "<li><div class='collapsible-header' style='color: #263238;'><span class='gql-anumber'>" + num + "</span>&nbsp;&nbsp;&nbsp;" + obj.data.User.favourites.anime.nodes[i].title.romaji + " (eng.: " + obj.data.User.favourites.anime.nodes[i].title.english + ")</div><div class='collapsible-body' style='background: #fff; color: #263238;'><div class='card'><div class='card-content' style='background: " + obj.data.User.favourites.anime.nodes[i].coverImage.color + "'><span class='card-title'>" + obj.data.User.favourites.anime.nodes[i].title.romaji +"</span></div><div class='card-content'><p><img width='17%' src='" + obj.data.User.favourites.anime.nodes[i].coverImage.extraLarge + "' style='overflow:auto; float:left; margin-right:1%;'>" + obj.data.User.favourites.anime.nodes[i].description.replace('\r\n','') + "<p></p><h6 style='text-decoration: underline;'>My notes on the anime:</h6><p id='gql-notes" + num + "'></p></p></div><div class='card-action'><a href='" + obj.data.User.favourites.anime.nodes[i].siteUrl + "'>Go to anime's page</a></div></div>"
+			document.getElementById("gql-alist").innerHTML += "<li><div class='collapsible-header' style='color: #263238;'><span class='gql-anumber'>" + num + "</span>&nbsp;&nbsp;&nbsp;" + obj.data.User.favourites.anime.nodes[i].title.romaji + " (eng.: " + obj.data.User.favourites.anime.nodes[i].title.english + ")</div><div class='collapsible-body' style='background: #fff; color: #263238;'><div class='card'><div class='card-content' style='background: " + obj.data.User.favourites.anime.nodes[i].coverImage.color + "'><span class='card-title'>" + obj.data.User.favourites.anime.nodes[i].title.romaji + "</span></div><div class='card-content'><p><img width='17%' src='" + obj.data.User.favourites.anime.nodes[i].coverImage.extraLarge + "' style='overflow:auto; float:left; margin-right:1%;'>" + obj.data.User.favourites.anime.nodes[i].description.replace('\r\n', '') + "<p></p><h6 style='text-decoration: underline;'>My notes on the anime:</h6><p id='gql-notes" + num + "'></p></p></div><div class='card-action'><a href='" + obj.data.User.favourites.anime.nodes[i].siteUrl + "'>Go to anime's page</a></div></div>"
+			num++;
 		}
 
 		i++;
-		num++;
-	} while (i != 10);
-	
+	} while (i != obj.data.User.favourites.anime.nodes.length);
+
 	num = 1;
-	for (var j = 0; j < obj.data.MediaListCollection.lists[1].entries.length; j++) {
-			for (var p = 0; p < 10; p++) {
-				if (obj.data.User.favourites.anime.nodes[p].id == obj.data.MediaListCollection.lists[1].entries[j].mediaId) {
-					var replaced = obj.data.MediaListCollection.lists[1].entries[j].notes.replace(/⭐/g, "⭐<br>");
-					var replaced = replaced.replace(/lists/g, "yeet");
-					var replaced = replaced.replace(/list./g, "list.<br>");
-					var replaced = replaced.replace(/yeet/g, "lists");
-					var replaced = replaced.replace(/Rating:/g, "<br><b>Rating:</b>");
-					document.getElementById("gql-notes" + num).innerHTML += replaced;
-					num++;
-				}
+	for (var p = 0; p < obj.data.User.favourites.anime.nodes.length; p++) {
+		for (var j = 0; j < obj.data.MediaListCollection.lists[1].entries.length; j++) {
+			if (obj.data.User.favourites.anime.nodes[p].id == obj.data.MediaListCollection.lists[1].entries[j].mediaId) {
+				var replaced = obj.data.MediaListCollection.lists[1].entries[j].notes.replace(/⭐/g, "⭐<br>");
+				replaced = replaced.replace(/lists/g, "yeet");
+				replaced = replaced.replace(/list./g, "list.<br>");
+				replaced = replaced.replace(/yeet/g, "lists");
+				replaced = replaced.replace(/Rating:/g, "<br><b>Rating:</b>");
+				document.getElementById("gql-notes" + num).innerHTML += replaced;
+				num++;
 			}
 		}
+	}
 }
 
 function handleError(error) {
